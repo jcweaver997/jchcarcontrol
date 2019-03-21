f9e21da1abf488fce8203481e95e188327b5e3a3
jc <jcweaver997@gmail.com>
2019-03-21 18:09:16 -0400

    Test version

---
+++ jchcarcontrol/JcRobotNetworking.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace jchcarcontrol
+{
+    class JcRobotNetworking
+    {
+        public enum ConnectionType
+        {
+            Robot, Controller
+        }
+
+        public enum DefaultCommands
+        {
+            ReceivedMessage
+        }
+
+        public struct Command
+        {
+            public byte commandID { get; set; }
+            public byte[] param { get; set; }
+            public Command(byte comid, byte[] para)
+            {
+                param = para;
+                commandID = comid;
+            }
+        }
+        private byte ReceivedMessage = 255;
+
+        private Socket socket;
+        private IPEndPoint other;
+        public delegate void OnCommandRecieve(Command c);
+        public ConnectionType connectionType { get; private set; }
+        private int port = 1296;
+        private const int maxMessageSize = 10;
+        private string robotHostName = "";
+        private const int timeout = 300;
+        public OnCommandRecieve onCommandRecieve { get; set; }
+        private Thread listenThread;
+        private bool listenerRunning;
+        private bool commandRecieved;
+        private byte[] lastCommandValue;
+        private bool connected = false;
+
+
+        public JcRobotNetworking(ConnectionType type, OnCommandRecieve onCommandRecieve = null)
+        {
+            connectionType = type;
+            this.onCommandRecieve = onCommandRecieve;
+            //Commands = new Dictionary<byte, byte>();
+            //Commands.Add(0,ReceivedMessage);
+        }
+
+        public void Connect(int port, string hostname = "")
+        {
+            this.port = port;
+            this.robotHostName = hostname;
+            switch (connectionType)
+            {
+                case ConnectionType.Controller:
+                    ConnectController();
+                    break;
+                case ConnectionType.Robot:
+                    ConnectRobot();
+                    break;
+                default:
+                    Console.WriteLine("Connection type not defined");
+                    break;
+            }
+
+        }
+        private IPAddress GetRobotIP()
+        {
+            IPAddress ret;
+            if(IPAddress.TryParse(robotHostName, out ret))
+            {
+                return ret;
+            }
+
+            IPHostEntry entry = null;
+            try
+            {
+                entry = Dns.GetHostEntry(robotHostName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Can't find the robot in dns");
+                Console.WriteLine("Trying again in 5 seconds...");
+                System.Threading.Thread.Sleep(5000);
+
+                return GetRobotIP();
+            }
+
+            if (entry.AddressList.Length <= 0)
+            {
+                Console.WriteLine("Can't find the robot in DNS");
+                Console.WriteLine("Trying again in 5 seconds...");
+                System.Threading.Thread.Sleep(5000);
+
+                return GetRobotIP();
+
+            }
+            Console.WriteLine("DNS found:");
+            int index = -1;
+            for (int i = 0; i < entry.AddressList.Length; i++)
+            {
+                if (entry.AddressList[i].AddressFamily == AddressFamily.InterNetworkV6)
+                {
+                    Console.WriteLine("ipv6: " + entry.AddressList[i].ToString());
+                }
+                else if (entry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    index = i;
+                    Console.WriteLine("ipv4: " + entry.AddressList[i].ToString());
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                Console.WriteLine("Can't find a good ip in dns");
+                Console.WriteLine("Trying again in 5 seconds...");
+                System.Threading.Thread.Sleep(5000);
+                return GetRobotIP();
+            }
+            Console.WriteLine("Found robots ip address " + entry.AddressList[index].ToString());
+            return entry.AddressList[index];
+        }
+
+        private void ConnectController()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress serverAddr = GetRobotIP();
+
+            other = new IPEndPoint(serverAddr, port);
+            socket.Connect(other);
+            listenThread = new Thread(ListenThread);
+            listenerRunning = true;
+            listenThread.Start();
+        }
+
+        private void ConnectRobot()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            other = new IPEndPoint(IPAddress.Any, port);
+            Console.WriteLine("waiting for connection from controller...");
+            listenThread = new Thread(ListenThread);
+            listenerRunning = true;
+            listenThread.Start();
+        }
+
+        private void ListenThread()
+        {
+            while (listenerRunning)
+            {
+                byte[] buffer = new byte[maxMessageSize];
+                EndPoint remote = (EndPoint)other;
+                int numReceived = 0;
+
+                if (connectionType == ConnectionType.Robot)
+                {
+                    remote = new IPEndPoint(IPAddress.Any, port);
+                    numReceived = socket.ReceiveFrom(buffer, ref remote);
+                    other = (IPEndPoint)remote;
+                }
+                else if (connectionType == ConnectionType.Controller)
+                {
+                    numReceived = socket.Receive(buffer);
+                }
+
+                if (numReceived != 5)
+                {
+                    Console.WriteLine("Packet size is wrong! " + numReceived + " from " + other.Address.ToString());
+                    continue;
+                }
+                connected = true;
+
+                try
+                {
+                    byte[] param = new byte[4];
+                    Array.Copy(buffer, 1, param, 0, 4);
+                    Command c = new Command(buffer[0], param);
+                    if (c.commandID == ReceivedMessage)
+                    {
+
+                        if (commandRecieved == false && CheckSum(lastCommandValue, c.param))
+                        {
+                            commandRecieved = true;
+                        }
+                    }
+                    else
+                    {
+                        onCommandRecieve(c);
+                        c.commandID = ReceivedMessage;
+                        SendCommand(c);
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error in message handling! " + e.Message);
+                }
+            }
+
+        }
+
+        private bool CheckSum(byte[] ar1, byte[] ar2)
+        {
+            int firstSum = 0, secondSum = 0;
+            for (int i = 0; i < Math.Min(ar1.Length, ar2.Length); i++)
+            {
+                firstSum += ar1[i];
+                secondSum += ar2[i];
+                firstSum %= 256;
+                secondSum %= 256;
+            }
+            return firstSum == secondSum;
+
+        }
+
+        public void SendCommand(Command c, bool waitForResponse = false)
+        {
+            byte[] buffer = new byte[5];
+            buffer[0] = c.commandID;
+            Array.Copy(c.param, 0, buffer, 1, 4);
+            commandRecieved = false;
+
+            if (connectionType == ConnectionType.Robot)
+            {
+                if (!connected)
+                {
+                    return;
+                }
+                socket.SendTo(buffer, other);
+            }
+            else if (connectionType == ConnectionType.Controller)
+            {
+                socket.Send(buffer);
+            }
+
+            lastCommandValue = c.param;
+
+            int counter = 0;
+            while (waitForResponse && !commandRecieved)
+            {
+                counter++;
+                Thread.Sleep(1);
+                if (counter > timeout)
+                {
+                    Console.WriteLine((connectionType == ConnectionType.Robot ? "Cont:" : "Robot:") + " No response" + "!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ jchcarcontrol/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jchcarcontrol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            new Thread(Loop).Start();
+        }
+
+        public void Loop()
+        {
+            Controller c = new Controller(UserIndex.One);
+            JcRobotNetworking net = new JcRobotNetworking(JcRobotNetworking.ConnectionType.Controller);
+            net.Connect(1296,"10.9.0.3");
+            if (c == null)
+            {
+                
+            }
+            while (c.IsConnected)
+            {
+                State s = c.GetState();
+                net.SendCommand(new JcRobotNetworking.Command(1, BitConverter.GetBytes(s.Gamepad.LeftThumbX / (float)short.MaxValue / 2f + .5f)));
+                net.SendCommand(new JcRobotNetworking.Command(2, BitConverter.GetBytes(((float)s.Gamepad.RightTrigger-s.Gamepad.LeftTrigger) / 255f)));
+                Console.WriteLine(s.Gamepad.LeftThumbX / (float)short.MaxValue / 2f + .5f);
+                Console.WriteLine(((float)s.Gamepad.RightTrigger - s.Gamepad.LeftTrigger) / 255f);
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
