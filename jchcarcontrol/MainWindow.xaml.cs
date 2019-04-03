5e21598952e7bc29c493ae5969d6c09f6c878b1f
jc <jcweaver997@gmail.com>
2019-04-03 19:44:00 -0400

    Wifi cracking and scanning now working

--- jchcarcontrol/MainWindow.xaml.cs
+++ jchcarcontrol/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using SharpDX.XInput;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,30 +25,116 @@ namespace jchcarcontrol
     /// </summary>
     public partial class MainWindow : Window
     {
+        TcpNetworking net;
+        private static MainWindow instance;
+        private Thread loopThread;
+
         public MainWindow()
         {
 
-            new Thread(Loop).Start();
         }
+
+        public void OnMessageReceive(string message)
+        {
+            Log(message);
+        }
+
+
+        public static void Log(string message)
+        {
+            instance.Dispatcher.Invoke(() =>
+            {
+                instance.textLog.AppendText(message + "\n");
+                instance.textLog.ScrollToEnd();
+            });
+        }
+
+
 
         public void Loop()
         {
             Controller c = new Controller(UserIndex.One);
-            JcRobotNetworking net = new JcRobotNetworking(JcRobotNetworking.ConnectionType.Controller);
-            net.Connect(1296,"10.9.0.3");
+            JcRobotNetworking jcRobotNetworking = new JcRobotNetworking(JcRobotNetworking.ConnectionType.Controller);
+            jcRobotNetworking.Connect(1296,"10.9.0.3");
+            
             if (c == null)
             {
-                
+                Log("controller not connected");
+            }else if (c.IsConnected)
+            {
+                Log("controller is connected");
+            }
+            else
+            {
+                Log("controller??");
             }
             while (c.IsConnected)
             {
                 State s = c.GetState();
-                net.SendCommand(new JcRobotNetworking.Command(1, BitConverter.GetBytes(s.Gamepad.LeftThumbX / (float)short.MaxValue / 2f + .5f)));
-                net.SendCommand(new JcRobotNetworking.Command(2, BitConverter.GetBytes(((float)s.Gamepad.RightTrigger-s.Gamepad.LeftTrigger) / 255f)));
+                jcRobotNetworking.SendCommand(new JcRobotNetworking.Command(1, BitConverter.GetBytes(s.Gamepad.LeftThumbX / (float)short.MaxValue / 2f + .5f)));
+                jcRobotNetworking.SendCommand(new JcRobotNetworking.Command(2, BitConverter.GetBytes(((float)s.Gamepad.RightTrigger-s.Gamepad.LeftTrigger) / 255f)));
                 Console.WriteLine(s.Gamepad.LeftThumbX / (float)short.MaxValue / 2f + .5f);
                 Console.WriteLine(((float)s.Gamepad.RightTrigger - s.Gamepad.LeftTrigger) / 255f);
                 Thread.Sleep(20);
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            net.Send("scan");
+            Log("sent scan request");
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            instance = this;
+            net = new TcpNetworking(new IPEndPoint(IPAddress.Parse("10.9.0.3"), 1296), OnMessageReceive);
+            new Thread(() => {
+                try
+                {
+                    net.Start();
+                    Log("Done connecting");
+                }
+                catch (SocketException se)
+                {
+                    Log("Error connecting: " + se.Message);
+                }
+
+
+            }).Start();
+            loopThread = new Thread(Loop);
+            loopThread.Start();
+            Log("Started ");
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            net.Close();
+            loopThread.Abort();
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            net.Send("crack "+crackNumber.Text);
+            Log("sent crack request");
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            net.Send("stop");
+            Log("sent stop request");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textLog.Text = "";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            net.Send("list");
+            Log("sent list request");
         }
     }
 }
---
+++ jchcarcontrol/TcpNetworking.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace jchcarcontrol
+{
+    public class TcpNetworking
+    {
+        private ConnectionType connectionType;
+        private IPEndPoint remoteEP;
+        private IPEndPoint localEP;
+        private TcpListener server;
+        private TcpClient client;
+        private OnMessageRecieve onMessageRecieve;
+        private Thread serverAcceptThread;
+        private Thread receiveThread;
+        private StreamReader sr;
+        private StreamWriter sw;
+
+        public delegate void OnMessageRecieve(string m);
+
+
+        private enum ConnectionType
+        {
+            Client, Server
+        }
+
+
+
+        public TcpNetworking(IPEndPoint ep, OnMessageRecieve onMessageRecieve = null)
+        {
+            this.onMessageRecieve = onMessageRecieve;
+            if (ep.Address.Equals(IPAddress.Any))
+            {
+                ServerInit(ep);
+            }
+            else
+            {
+                ClientInit(ep);
+            }
+        }
+
+        private void ServerInit(IPEndPoint ep)
+        {
+            connectionType = ConnectionType.Server;
+            localEP = ep;
+        }
+
+        private void ClientInit(IPEndPoint ep)
+        {
+            connectionType = ConnectionType.Client;
+            remoteEP = ep;
+
+        }
+
+        public void Start()
+        {
+            if (connectionType == ConnectionType.Server)
+            {
+                ServerStart();
+            }
+            else
+            {
+                ClientStart();
+            }
+        }
+
+        private void ServerAccept()
+        {
+            while (true)
+            {
+
+                TcpClient newclient = server.AcceptTcpClient();
+                Console.WriteLine("New TCP Client at " + newclient.Client.RemoteEndPoint);
+                client?.Close();
+                client = newclient;
+                sr = new StreamReader(client.GetStream());
+                sw = new StreamWriter(client.GetStream());
+                receiveThread?.Abort();
+                receiveThread = new Thread(ReceiveThread);
+                receiveThread.Start();
+            }
+        }
+
+        private void ReceiveThread()
+        {
+            if (onMessageRecieve == null) return;
+            string message;
+            while (true)
+            {
+                message = sr.ReadLine();
+                if (message == null || sr.EndOfStream)
+                {
+                    break;
+                }
+                onMessageRecieve(message);
+            }
+        }
+
+        private void ServerStart()
+        {
+            server = new TcpListener(localEP);
+            server.Start();
+            serverAcceptThread = new Thread(ServerAccept);
+            serverAcceptThread.Start();
+            
+        }
+
+        private void ClientStart()
+        {
+            client = new TcpClient();
+            client.Connect(remoteEP);
+            sr = new StreamReader(client.GetStream());
+            sw = new StreamWriter(client.GetStream());
+            receiveThread?.Abort();
+            receiveThread = new Thread(ReceiveThread);
+            receiveThread.Start();
+
+        }
+
+        public void Close()
+        {
+            client?.Close();
+            serverAcceptThread?.Abort();
+            receiveThread?.Abort();
+        }
+
+        public void Send(string message)
+        {
+            sw?.WriteLine(message);
+            sw?.Flush();
+        }
+
+
+
+
+
+    }
+}
